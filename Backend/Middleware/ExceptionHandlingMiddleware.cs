8c36a0bedc16bc43368e5b1c10f382615141ed83
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2024-04-24 22:34:12 +0300

    feat: added ExceptionHandling middleware v1 (#3)
    exception Handling middleware v1

---
+++ Backend/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Backend.Models;
+using System.Net;
+using System.Net.Mime;
+using System.Text.Json;
+
+namespace Backend.Middleware;
+
+public class ExceptionHandlingMiddleware : IMiddleware
+{
+    private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+    private readonly IHostEnvironment _enviroment;
+
+    public ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger, IHostEnvironment environment)
+    {
+        _logger = logger;
+        _enviroment = environment;
+    }
+
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, exception.Message);
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var response = _enviroment.IsDevelopment()
+               ? new CustomResponse(context.Response.StatusCode, exception.Message, exception?.StackTrace?.ToString())
+               : new CustomResponse(context.Response.StatusCode, "Internal Server Error");
+
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        var json = JsonSerializer.Serialize(response, options);
+        await context.Response.WriteAsync(json);
+
+    }
+}
---
+++ Backend/Models/CustomResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Backend.Models;
+
+public class CustomResponse
+{
+    public int StatusCode { get; set; }
+    public string? Message { get; set; }
+    public string? Details { get; set; }
+    public CustomResponse(int statusCode, string message, string? details)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+
+    public CustomResponse(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        Message = message;
+    }
+}
