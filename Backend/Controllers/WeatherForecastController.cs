c28c195a5381a858c97481b9476ce12ccfe1a798
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2024-05-01 21:09:56 +0300

    feat: added versioning to apis and swagger configuration

--- Backend/Controllers/WeatherForecastController.cs
+++ Backend/Controllers/WeatherForecastController.cs
@@ -5,7 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace Backend.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiVersion("1.0")]
 public class WeatherForecastController : ControllerBase
 {
     private readonly ILogger<WeatherForecastController> _logger;
@@ -17,12 +18,13 @@ public class WeatherForecastController : ControllerBase
         _mediator = mediator;
     }
 
+    [MapToApiVersion("1.0")] // map each action to a specific version
     [HttpGet(Name = "GetWeatherForecast")]
     public async Task<IActionResult> Get([FromQuery] GetWeatherForecastQuery query, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Weather Forecast executing...");
         var result = await _mediator.Send(query, cancellationToken);
 
-       return Ok(result);
+        return Ok(result);
     }
 }
