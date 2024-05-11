93238291be4efb9ef8919202fdae92694214172d
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2024-05-11 23:34:46 +0300

    feat: added baseResponse model for output of controllers

--- Backend/CORS/Queries/GetWeatherForecastQuery.cs
+++ Backend/CORS/Queries/GetWeatherForecastQuery.cs
@@ -3,11 +3,11 @@ using MediatR;
 
 namespace Backend.CORS.Queries;
 
-public class GetWeatherForecastQuery : IRequest<List<WeatherForecast>>
+public class GetWeatherForecastQuery : IRequest< BaseResponse<List<WeatherForecast>>>
 {
     public int MaxNumberOfForecastToReturn { get; init; }
 }
-public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecastQuery, List<WeatherForecast>>
+public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecastQuery, BaseResponse<List<WeatherForecast>>>
 {
     private static readonly string[] Summaries = new[] {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -16,7 +16,7 @@ public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecast
     {
     }
 
-    public Task<List<WeatherForecast>> Handle(GetWeatherForecastQuery request, CancellationToken cancellationToken)
+    public async Task<BaseResponse<List<WeatherForecast>>> Handle(GetWeatherForecastQuery request, CancellationToken cancellationToken)
     {
         var resultTask = Task<List<WeatherForecast>>.Factory.StartNew(() =>
         {
@@ -33,7 +33,8 @@ public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecast
 
             return output;
         });
+        var output = await resultTask;
 
-        return resultTask;
+        return new BaseResponse<List<WeatherForecast>>(output, System.Net.HttpStatusCode.OK);
     }
 }
