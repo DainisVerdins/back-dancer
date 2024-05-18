c4b327203294736b94499671d4b0132ce615ccab
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2024-05-18 23:47:47 +0300

    feat: Exception class to return to client for Bad requests

--- Backend/Middleware/ExceptionHandlingMiddleware.cs
+++ Backend/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,5 +1,4 @@
 ﻿using Backend.Models;
-using MediatR;
 using System.Net;
 using System.Net.Mime;
 using System.Text.Json;
@@ -35,12 +34,11 @@ public class ExceptionHandlingMiddleware : IMiddleware
         context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
         var response = _enviroment.IsDevelopment()
-               ? new BaseResponse<Unit>(HttpStatusCode.InternalServerError, exception.Message, exception?.StackTrace?.ToString())
-               : new BaseResponse<Unit>(HttpStatusCode.InternalServerError, "Internal Server Error", null);
+               ? new CustomExceptionResponse(HttpStatusCode.InternalServerError, exception.Message, exception?.StackTrace?.ToString())
+               : new CustomExceptionResponse(HttpStatusCode.InternalServerError, "Internal Server Error");
 
         var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
         var json = JsonSerializer.Serialize(response, options);
         await context.Response.WriteAsync(json);
-
     }
 }
---
+++ Backend/Models/CustomExceptionResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Backend.Models;
+
+public class CustomExceptionResponse
+{
+    public string ErrorMessage { get; set; } = null!;
+    public string? Details { get; set; } = null!;
+    public HttpStatusCode StatusCode { get; set; }
+
+    public CustomExceptionResponse(HttpStatusCode statusCode, string errorMessage, string? errorMessageDetails)
+    {
+        ErrorMessage = errorMessage;
+        StatusCode = statusCode;
+        Details = errorMessageDetails;
+    }
+    public CustomExceptionResponse(HttpStatusCode statusCode, string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+        StatusCode = statusCode;
+    }
+}
