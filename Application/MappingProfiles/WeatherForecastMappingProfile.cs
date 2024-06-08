ffc530bf2f73efd6b3649a4c8da36c7700340277
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2024-06-08 22:54:49 +0300

    feat: first version of clear architecture for project

---
+++ Application/MappingProfiles/WeatherForecastMappingProfile.cs
@@ -0,0 +1,10 @@
+﻿using AutoMapper;
+
+namespace Application.MappingProfiles;
+
+internal class WeatherForecastMappingProfile : Profile
+{
+    public WeatherForecastMappingProfile()
+    {
+    }
+}
