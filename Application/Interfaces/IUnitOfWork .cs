55e2303d32abdb60e9336dd50f19fddbe33d96ef
DainVerd <zobs10@inbox.lv>
2025-06-17 19:18:57 +0300

    added minor change

--- Application/Interfaces/IUnitOfWork .cs
+++ Application/Interfaces/IUnitOfWork .cs
@@ -5,6 +5,7 @@ namespace Application.Interfaces;
 public interface IUnitOfWork : IDisposable
 {
     IRefreshTokenRepository RefreshTokens { get; }
+
     int SaveChanges();
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
--- Infrastructure/Persistance/UnitOfWork/UnitOfWork.cs
+++ Infrastructure/Persistance/UnitOfWork/UnitOfWork.cs
@@ -29,5 +29,6 @@ public class UnitOfWork : IUnitOfWork
     public void Dispose()
     {
         _context.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
