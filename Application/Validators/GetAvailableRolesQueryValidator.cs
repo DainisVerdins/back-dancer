7dcdc27cdb6494a9d2145c361172006f53066d0f
DainVerd <zobs10@inbox.lv>
2026-06-10 16:42:58 +0300

    added validators

---
+++ Application/Validators/GetAvailableRolesQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.CORS.Queries;
+using FluentValidation;
+
+namespace Application.Validators;
+
+internal class GetAvailableRolesQueryValidator : AbstractValidator<GetAvailableRolesQuery>
+{
+    public GetAvailableRolesQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+                .NotEmpty();
+    }
+}
