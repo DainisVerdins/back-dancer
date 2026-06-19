91581ac75a04c85880e33c5685a152b29f60b050
Dain Verd <55252915+DainVerd@users.noreply.github.com>
2026-06-19 13:43:08 +0300

    feat:  create animal entities (#34)

---
+++ Domain/Models/Animal.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Enums;
+using Domain.Interfaces;
+
+namespace Domain.Models;
+
+public class Animal : IEntity
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    public string? Breed { get; set; }
+    public string Description { get; set; } = string.Empty;
+
+    public Gender Gender { get; set; }
+    public AnimalSize Size { get; set; }
+    public Temperament Temperament { get; set; }
+    public AnimalStatus Status { get; set; } = AnimalStatus.SearchingForHome;
+
+    public DateTime? DateOfBirth { get; set; }
+    public DateTime AdmissionDate { get; set; } = DateTime.UtcNow;
+
+    public bool IsSterilized { get; set; }
+    public bool IsVaccinated { get; set; }
+
+    public List<AnimalImage> Images { get; set; } = [];
+}
---
+++ Domain/Models/AnimalImage.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces;
+
+namespace Domain.Models;
+
+public class AnimalImage : IEntity
+{
+    public int Id { get; set; }
+    public string Url { get; set; } = string.Empty; // url to s3 bucket
+    public bool IsMain { get; set; }
+
+    public int AnimalId { get; set; }
+    public Animal? Animal { get; set; }
+}
